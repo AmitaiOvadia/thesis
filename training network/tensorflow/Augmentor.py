c166e1876878aa3ce6458e4ae9e95945ab2376a7
AmitaiOvadia <101287679+AmitaiOvadia@users.noreply.github.com>
2024-11-25 10:58:49 +0200

    Add files via upload

--- training network/tensorflow/Augmentor.py
+++ training network/tensorflow/Augmentor.py
@@ -1,112 +1,149 @@
 import numpy as np
-from scipy.ndimage import shift
+from scipy.ndimage import shift, rotate
 from PIL import Image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# import tensorflow as tf
+import torch
+from constants import *
 import cv2
 
 
 class Augmentor:
-    def __init__(self,
-                 config, number_of_input_channels, number_of_output_channels):
-        self.use_custom_function = bool(config["custom"])
-        self.xy_shifts = config["augmentation shift x y"]
-        self.rotation_range = config["rotation range"]
-        self.seed = config["seed"]
-        self.batch_size = config["batch_size"]
-        self.zoom_range = config["zoom range"]
-        self.interpolation_order = config["interpolation order"]
-        self.do_horizontal_flip = bool(config["horizontal flip"])
-        self.do_vertical_flip = bool(config["vertical flip"])
-        self.shear_range = config["shear_range"]
-        self.number_of_input_channels = number_of_input_channels
-        self.number_of_output_channels = number_of_output_channels
-        self.custom_augmentation_function = self.get_custom_augmentation_function()
-        self.debug_mode = bool(config["debug mode"])
+    @staticmethod
+    def scale_image(img, scale):
+        if scale != 1:
+            height, width = img.shape[:2]  # Get the height and width of the image
+            center = (width / 2, height / 2)  # Get the center of the image
 
-        if self.debug_mode:
-            self.batch_size = 1
+            # Generate a rotation matrix with no rotation, only scaling
+            rotation_matrix = cv2.getRotationMatrix2D(center, angle=0, scale=scale)
 
-    def get_data_generator(self, box, confmaps):
-        datagen = self.config_data_generator(box, confmaps)
-        return datagen
-
-    def config_data_generator(self, box, confmaps):
-        if self.use_custom_function:
-            data_gen_args = dict(preprocessing_function=self.custom_augmentation_function)
+            # Apply the transformation (rotation and scaling)
+            zoomed_img = cv2.warpAffine(img, rotation_matrix, (width, height), flags=cv2.INTER_CUBIC,
+                                        borderMode=cv2.BORDER_CONSTANT, borderValue=0)
         else:
-            data_gen_args = dict(rotation_range=self.rotation_range,
-                                 zoom_range=self.zoom_range,
-                                 horizontal_flip=self.do_horizontal_flip,
-                                 vertical_flip=self.do_vertical_flip,
-                                 width_shift_range=self.xy_shifts,
-                                 height_shift_range=self.xy_shifts,
-                                 interpolation_order=self.interpolation_order,
-                                 shear_range=self.shear_range)
-
-        datagen_x = ImageDataGenerator(**data_gen_args)
-        datagen_y = ImageDataGenerator(**data_gen_args)
-
-        datagen_x.fit(box, augment=True, seed=self.seed)
-        datagen_y.fit(confmaps, augment=True, seed=self.seed)
-
-        flow_box = datagen_x.flow(box, batch_size=self.batch_size, seed=self.seed, shuffle=True)
-        flow_conf = datagen_y.flow(confmaps, batch_size=self.batch_size, seed=self.seed, shuffle=True)
-
-        return self.custom_generator(flow_box, flow_conf)
-
-    def custom_generator(self, flow_box, flow_conf):
-        while True:
-            box_batch = next(flow_box)
-            conf_batch = next(flow_conf)
-            yield box_batch, conf_batch
+            zoomed_img = img
+        # plt.figure()
+        # plt.imshow(zoomed_img)
+        # plt.axis('equal')
+        # plt.show()
+        return zoomed_img
 
     @staticmethod
     def augment(img, h_fl, v_fl, rotation_angle, shift_y_x, scale_factor):
-        if np.max(img) <= 1:
-            img = np.uint8(img * 255)
         if h_fl:
-            img = np.fliplr(img)
+            img = np.fliplr(np.copy(img))
         if v_fl:
-            img = np.flipud(img)
+            img = np.flipud(np.copy(img))
         if scale_factor != 1:
-            img = Augmentor.scale(img, scale_factor=scale_factor)
-        img = shift(img, shift_y_x)
-        img_pil = Image.fromarray(img)
-        img_pil = img_pil.rotate(rotation_angle, 3)
-        img = np.asarray(img_pil)
-        if np.max(img) > 1:
-            img = img / 255
+            img = Augmentor.scale_image(np.copy(img), scale_factor)
+        if not np.all(shift_y_x == 0):
+            img = shift(np.copy(img), shift_y_x)
+        if rotation_angle != 0:
+            img = Augmentor.rotate_scipy(np.copy(img), rotation_angle)
         return img
 
     @staticmethod
-    def scale(img, scale_factor):
-        N = img.shape[0]
-        scale_factor = float(scale_factor)
-        center = (N / 2, N / 2)
-        zoom_matrix = cv2.getRotationMatrix2D(center, 0, scale_factor)
-        zoomed_img = cv2.warpAffine(img, zoom_matrix, (N, N), flags=cv2.INTER_CUBIC)
-        return zoomed_img
+    def shift_image(img, shift_y_x):
+        shift_x, shift_y = shift_y_x
+        height, width = img.shape
 
-    def get_custom_augmentation_function(self):
-        rotation_range = self.rotation_range
-        xy_shift = self.xy_shifts
-        can_horizontal_flip = self.do_horizontal_flip
-        can_vertical_flip = self.do_vertical_flip
-        zoom_range = self.zoom_range
-        number_of_input_channels = self.number_of_input_channels
-        number_of_output_channels = self.number_of_output_channels
+        # Find non-zero pixel indices
+        nz_y, nz_x = np.nonzero(img)
 
-        def custom_augmentations(img):
-            new_image = np.zeros_like(img)
-            do_horizontal_flip = bool(np.random.randint(2)) and can_horizontal_flip
-            do_vertical_flip = bool(np.random.randint(2)) and can_vertical_flip
-            rotation_angle = np.random.randint(-rotation_range, rotation_range)
-            shift_y_x = np.random.randint(-xy_shift, xy_shift, 2)
-            random_scale = np.random.uniform(low=zoom_range[0], high=zoom_range[1], size=1)
-            num_channels = img.shape[-1]
-            for channel in range(num_channels):
-                new_image[:, :, channel] = Augmentor.augment(np.copy(img[:, :, channel]), do_horizontal_flip,
-                                                             do_vertical_flip, rotation_angle, shift_y_x, random_scale)
-            return new_image
+        # If there are no non-zero pixels, return the original image
+        if len(nz_y) == 0 or len(nz_x) == 0:
+            return img
 
-        return custom_augmentations
+        # Calculate boundaries of the non-zero pixels
+        min_y, max_y = np.min(nz_y), np.max(nz_y)
+        min_x, max_x = np.min(nz_x), np.max(nz_x)
+
+        # Calculate the maximum allowable shift
+        max_shift_y_down = height - 1 - max_y
+        max_shift_y_up = -min_y
+        max_shift_x_right = width - 1 - max_x
+        max_shift_x_left = -min_x
+
+        # Adjust shift_y and shift_x to stay within bounds
+        shift_y = min(max(shift_y, max_shift_y_up), max_shift_y_down)
+        shift_x = min(max(shift_x, max_shift_x_left), max_shift_x_right)
+
+        shifted_image = np.zeros_like(img)
+
+        # Calculate new indices after the shift
+        new_y = np.clip(nz_y + shift_y, 0, height - 1)
+        new_x = np.clip(nz_x + shift_x, 0, width - 1)
+
+        # Place the non-zero values into the new positions
+        shifted_image[new_y, new_x] = img[nz_y, nz_x]
+        # from matplotlib import pyplot as plt
+        # plt.imshow(img*0.5 + shifted_image)
+        # plt.show()
+        return shifted_image
+
+    @staticmethod
+    def rotate(img, rotation_angle):
+        img_pil = Image.fromarray(img)
+        img_pil = img_pil.rotate(rotation_angle, 3)
+        img = np.asarray(img_pil)
+        return img
+
+    @staticmethod
+    def rotate_scipy(img, rotation_angle):
+        rotated_image = rotate(img, rotation_angle, reshape=False, order=3)
+        return rotated_image
+
+    @staticmethod
+    def custom_augmentations(img, rotation_angle, shift_y_x, do_horizontal_flip, do_vertical_flip, scaling):
+        num_channels = img.shape[-1]
+        augmented_image = np.zeros_like(img)
+        for channel in range(num_channels):
+            augmented_image[:, :, channel] = Augmentor.augment(np.copy(img[:, :, channel]), do_horizontal_flip,
+                                                   do_vertical_flip, rotation_angle, shift_y_x, scaling)
+        return augmented_image
+
+    @staticmethod
+    def tf_find_peaks(x):
+        """
+        Finds the maximum value in each channel and returns the location and value.
+        Args:
+            x: rank-4 tensor (samples, height, width, channels)
+
+        Returns:
+            peaks: rank-3 tensor (samples, [x, y], channels)
+        """
+        # Store input shape
+        if isinstance(x, np.ndarray):
+            x = torch.tensor(x)
+
+            # Ensure x is on the correct device
+        x = x.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+
+        # Store input shape
+        in_shape = x.shape
+
+        image_size = int(in_shape[1])
+
+        # Flatten height/width dims
+        flattened = x.reshape(in_shape[0], in_shape[1] * in_shape[2], in_shape[3])
+
+        # Find peaks in linear indices
+        vals, idx = torch.max(flattened, dim=1)
+
+        # Convert linear indices to subscripts
+        rows = idx // in_shape[2]
+        cols = idx % in_shape[2]
+
+        # Return N x 3 x C tensor
+        pred = torch.stack([
+            cols.float(),
+            rows.float(),
+            vals
+        ], dim=1)
+
+        pred = pred.permute(0, 2, 1)
+        pred = pred[..., :2]
+        # pred = pred / image_size  # normalize points
+
+        return pred
+
